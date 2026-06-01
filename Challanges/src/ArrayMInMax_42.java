f741b597667d185dbdd21c6b9ba4044ab3317fab
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-01 12:14:36 +0530

    Added Array Min & Max File

---
+++ Challanges/src/ArrayMInMax_42.java
@@ -0,0 +1,19 @@
+public class ArrayMInMax_42 {
+    public static void main(String[] args) {
+        int[] numArr = ArrayUtility.inputArray();
+        int Min = numArr[0];
+        int Max = numArr[0];
+
+        int i = 0;
+        while (i < numArr.length){
+            if(numArr[i] < Min){
+                Min = numArr[i];
+            } else if (numArr[i] > Max) {
+                Max = numArr[i];
+            }
+            i++;
+        }
+        System.out.println("Mininum Array is : " + Min);
+        System.out.println("Maimum Array is : " + Max);
+    }
+}
