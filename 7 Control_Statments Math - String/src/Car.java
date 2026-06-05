223a00fc1c9f1417b8868cf576c91b80ffa8867b
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-05 11:59:42 +0530

    Added toString method logic

---
+++ 7 Control_Statments Math - String/src/Car.java
@@ -0,0 +1,43 @@
+public class Car {
+
+    int noOfWheels;
+
+    int noOfDoors;
+
+    int maxSpeed;
+
+    String name;
+
+    String modelName;
+
+    String company;
+
+    public Car(int noOfWheels, int noOfDoors, int maxSpeed, String name, String modelName, String company) {
+        this.noOfWheels = noOfWheels;
+        this.noOfDoors = noOfDoors;
+        this.maxSpeed = maxSpeed;
+        this.name = name;
+        this.modelName = modelName;
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "noOfWheels=" + noOfWheels +
+                ", noOfDoors=" + noOfDoors +
+                ", maxSpeed=" + maxSpeed +
+                ", name='" + name + '\'' +
+                ", modelName='" + modelName + '\'' +
+                ", company='" + company + '\'' +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        Car swift = new Car(4,4,120,
+                "Swift","SWA96","Maruti");
+        System.out.println(swift.toString());
+        System.out.println(swift);
+    }
+}
