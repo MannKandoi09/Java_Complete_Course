4acfa134254d36845b685862a6b68b7765ffa6d8
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-06 18:31:23 +0530

    Count No of Digits

---
+++ McaInterviewPrep/src/NumberChallanges/CountDigits.java
@@ -0,0 +1,18 @@
+package NumberChallanges;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Number to count its total digits: ");
+        int num = scan.nextInt();
+        int count = 0;
+
+        while (num > 0){
+            count++;
+            num /= 10;
+        }
+        System.out.println("Total Count: " + count);
+    }
+}
