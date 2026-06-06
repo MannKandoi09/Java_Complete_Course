7093548df345a447d6c05d49542158b1253058d1
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-06 12:35:41 +0530

    Added Access Modifiers Logic

---
+++ Ch8 Encapsulation and Inheritance/src/in/knowledgegate/DefaultTest.java
@@ -0,0 +1,10 @@
+package in.knowledgegate;
+
+import in.kgcoding.Car;
+
+public class DefaultTest {
+    public static void main(String[] args) {
+        Car car = new Car("Yellow", "Dezire",5,3000);
+//        car.costOfPurchase = 1000;
+    }
+}
