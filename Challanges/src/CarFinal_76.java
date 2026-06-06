f326c0c96f41509ef1adab177943f62bfdbb6e49
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-06 10:54:41 +0530

    Final Keyword Challange added

---
+++ Challanges/src/CarFinal_76.java
@@ -0,0 +1,19 @@
+public class CarFinal_76 {
+
+    final int noOfWheels;
+
+    final String model;
+
+    final String engineInLiters;
+
+    public CarFinal_76(int noOfWheels, String model, String engineInLiters) {
+        this.noOfWheels = noOfWheels;
+        this.model = model;
+        this.engineInLiters = engineInLiters;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
