d18eb8738c5469764eb77067cb08f00b21531af5
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-06 18:41:46 +0530

    Strong Number

---
+++ McaInterviewPrep/src/NumberChallanges/StrongNumber.java
@@ -0,0 +1,29 @@
+package NumberChallanges;
+
+import java.util.Scanner;
+
+public class StrongNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number to find it is Strong ot not: ");
+        int num = scan.nextInt();
+        int temp = num;
+        int sum = 0;
+        while (num > 0){
+            int digit = num % 10;
+
+            int fact = 1;
+            for (int i = 1; i <= digit; i++){
+                fact *= i;
+            }
+
+            sum += fact;
+            num /= 10;
+        }
+        if (temp == sum){
+            System.out.println("It is a Strong Number..." + sum);
+        }else{
+            System.out.println("Not a Strong Number.." + sum);
+        }
+    }
+}
