fb253abac4185d1a6906678bed3b2c8e57b838ae
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-07 08:09:36 +0530

    Encapsulation Challange added

---
+++ Challanges/src/in/mkcoding/challange78/BankAccount.java
@@ -0,0 +1,37 @@
+package in.mkcoding.challange78;
+
+class BankAccount {
+
+    private String accountNumber;
+
+    private String accountHolderName;
+
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void depositMoney(double money){
+        if (money <= 0){
+            System.out.println("Invalid Deposit...");
+        }else{
+            balance += money;
+        }
+
+    }
+
+    public double withdrawMoney(double money){
+        if (balance >= money){
+            balance -= money;
+        } else if (balance >= money) {
+            balance -= money;
+        }else{
+            money = balance;
+            balance = 0;
+        }
+
+        return money;
+    }
+}
