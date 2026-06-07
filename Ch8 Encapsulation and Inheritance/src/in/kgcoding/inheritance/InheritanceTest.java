4303a2ae770c50ced91ac6079622ab644c11302d
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-07 10:41:14 +0530

    Object class logic

--- Ch8 Encapsulation and Inheritance/src/in/kgcoding/inheritance/InheritanceTest.java
+++ Ch8 Encapsulation and Inheritance/src/in/kgcoding/inheritance/InheritanceTest.java
@@ -3,14 +3,18 @@ package in.kgcoding.inheritance;
 public class InheritanceTest {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
-        vehicle.commute();
+//        vehicle.commute();
 
-        TwoWheeler twoVeh = new TwoWheeler();
-        twoVeh.commute();
-        twoVeh.balance();
-        MotorCycle mc = new MotorCycle();
-        mc.commute();
-        mc.balance();
-        mc.start();
+        System.out.println(vehicle.toString());
+        System.out.println(vehicle.hashCode());
+        System.out.println(vehicle.getClass());
+//
+//        TwoWheeler twoVeh = new TwoWheeler();
+//        twoVeh.commute();
+//        twoVeh.balance();
+//        MotorCycle mc = new MotorCycle();
+//        mc.commute();
+//        mc.balance();
+//        mc.start();
     }
 }
