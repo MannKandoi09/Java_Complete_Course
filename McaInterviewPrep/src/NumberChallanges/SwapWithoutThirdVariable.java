e1fbeef24d04c0cb648c0cbd137fa5b2caee3b64
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-09 07:46:13 +0530

    Swap Without third variable challange

---
+++ McaInterviewPrep/src/NumberChallanges/SwapWithoutThirdVariable.java
@@ -0,0 +1,15 @@
+package NumberChallanges;
+
+public class SwapWithoutThirdVariable {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Value of A is: " + a);
+        System.out.println("Value of B is: " + b);
+    }
+}
