e3afe6503c5a6ae637e1f545041c4ff2876e2809
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-10 10:09:07 +0530

    Sorting an Array in Ascending order

---
+++ McaInterviewPrep/src/ArrayChallanges/ArrayAscending.java
@@ -0,0 +1,12 @@
+package ArrayChallanges;
+
+import java.util.Arrays;
+
+public class ArrayAscending {
+    public static void main(String[] args) {
+        int[] arr ={1,5,4,3};
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
