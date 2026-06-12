2a62bafac0cdda2b085e0cacd8e3111ad87ffd2b
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-12 08:23:08 +0530

    Counting Characters in a String

---
+++ McaInterviewPrep/src/StringChallanges/countCharacters.java
@@ -0,0 +1,9 @@
+package StringChallanges;
+
+public class countCharacters {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Counting Characters in a String..\n");
+        String str = "mann";
+        System.out.println(str.length());
+    }
+}
