d570bdde1819dae7e123bde03248f0074940e533
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-13 12:55:05 +0530

    Removeing space from an string

---
+++ McaInterviewPrep/src/StringChallanges/removeSpaces.java
@@ -0,0 +1,10 @@
+package StringChallanges;
+
+public class removeSpaces {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Removing space from an string..\n");
+        String str = "Java is easy";
+        str = str.replace(" ", " ");
+        System.out.println(str);
+    }
+}
