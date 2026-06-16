6ee4e6a85f2a4b8761a2f5d199a59d3dad7e5d7d
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-16 09:57:37 +0530

    Replacing Elements in a String

---
+++ McaInterviewPrep/src/StringChallanges/ReplaceCharacters.java
@@ -0,0 +1,10 @@
+package StringChallanges;
+
+public class ReplaceCharacters {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Replacing a Character in a String..\n");
+        String str = "java";
+        str = str.replace('a' , 'A');
+        System.out.println(str);
+    }
+}
