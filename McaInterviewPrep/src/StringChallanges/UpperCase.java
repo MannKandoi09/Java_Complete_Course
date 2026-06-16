0e809be6ceefdc47e2f11048feefffc453e2730e
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-16 10:00:05 +0530

    Converting String into UpperCase

---
+++ McaInterviewPrep/src/StringChallanges/UpperCase.java
@@ -0,0 +1,9 @@
+package StringChallanges;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        System.out.println("Welcome to converting String into UpperCase..\n");
+        String str  = "mann";
+        System.out.println(str.toUpperCase());
+    }
+}
