1bb6fb59d0bf42f7feb5840c4184c451ba65abd3
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-16 10:01:52 +0530

    Converting String into Lowercase

---
+++ McaInterviewPrep/src/StringChallanges/LowerCase.java
@@ -0,0 +1,9 @@
+package StringChallanges;
+
+public class LowerCase {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Converting a String into Lower case..\n");
+        String str = "MANN";
+        System.out.println(str.toLowerCase());
+    }
+}
