940dca1e1d31c1c67ba2f8c4b9619e8fb8139a5e
MannKandoi09 <mannkandoi2005@gmail.com>
2026-06-21 16:27:29 +0530

    Longest Word in a String

---
+++ McaInterviewPrep/src/StringChallanges/LongestWord.java
@@ -0,0 +1,18 @@
+package StringChallanges;
+
+public class LongestWord {
+    public static void main(String[] args) {
+        String str = "Java programming is easy";
+
+        String words[] = str.split(" ");
+
+        String longest = "";
+
+        for(String word : words){
+            if(word.length() > longest.length())
+                longest = word;
+        }
+
+        System.out.println(longest);
+    }
+}
