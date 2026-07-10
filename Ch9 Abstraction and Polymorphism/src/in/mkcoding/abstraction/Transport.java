63aaa9877759ce2ae443444a92ee2ffb3683015e
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-10 10:03:34 +0530

    Interfaces Logic added

---
+++ Ch9 Abstraction and Polymorphism/src/in/mkcoding/abstraction/Transport.java
@@ -0,0 +1,6 @@
+package in.mkcoding.abstraction;
+
+public interface Transport {
+
+     void getSetGo();
+}
--- Ch9 Abstraction and Polymorphism/src/in/mkcoding/abstraction/Vehicle.java
+++ Ch9 Abstraction and Polymorphism/src/in/mkcoding/abstraction/Vehicle.java
@@ -1,8 +1,12 @@
 package in.mkcoding.abstraction;
 
-public class Vehicle {
+abstract class Vehicle implements Transport {
 
     private int noOfTires;
 
-    
+
+    @Override
+    public void getSetGo() {
+        System.out.println("going to place");
+    }
 }
