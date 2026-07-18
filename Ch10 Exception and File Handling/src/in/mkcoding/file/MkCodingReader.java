b2d2be3ddef0f1348e3068bef1ae874505bed114
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-18 13:03:34 +0530

    FileReader Concept

---
+++ Ch10 Exception and File Handling/src/in/mkcoding/file/MkCodingReader.java
@@ -0,0 +1,24 @@
+package in.mkcoding.file;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MkCodingReader {
+    public static void main(String[] args) {
+
+        String fileName = "java-course.txt";
+
+        try (FileReader reader = new FileReader(fileName)){
+
+            int read = 0;
+            do{
+                read = reader.read();
+                System.out.print((char)read);
+            }while (read != -1);
+
+        }catch (IOException e){
+            System.out.printf("Exception occurred: 5s", e.getMessage());
+
+        }
+    }
+}
