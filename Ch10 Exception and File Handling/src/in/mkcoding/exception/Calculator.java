a3081ad68c19105932dec1a9f10d3b9e6c09da04
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-18 10:41:09 +0530

    Try-Catch Block Added

---
+++ Ch10 Exception and File Handling/src/in/mkcoding/exception/Calculator.java
@@ -0,0 +1,23 @@
+package in.mkcoding.exception;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Division Calculator");
+        System.out.println("Please Enter your two Numbers: ");
+        int first = scan.nextInt();
+        int second = scan.nextInt();
+
+        try{
+            int result = first / second;
+            System.out.printf("Result is %d", result);
+        }catch (ArithmeticException exception){
+
+            System.out.printf("%s, Enter Valid Values",
+                    exception.getMessage());
+        }
+
+    }
+}
