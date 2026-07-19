6b6bc75237fe06494c58cd12fc4bec6b32bf4c17
MannKandoi09 <mannkandoi2005@gmail.com>
2026-07-19 12:10:29 +0530

    Wrapper Class Concept

--- Ch11 Collections And Generics/src/in/mkcoding/varargs/VarArgumentss.java
+++ Ch11 Collections And Generics/src/in/mkcoding/varargs/VarArgumentss.java
@@ -1,7 +1,7 @@
 package in.mkcoding.varargs;
 
 class VarArguments {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(sum(4,5));
         System.out.println(sum(4,5,1));
     }
---
+++ Ch11 Collections And Generics/src/in/mkcoding/wrapper/TestingWrapper.java
@@ -0,0 +1,9 @@
+package in.mkcoding.wrapper;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+
+        Integer first = Integer.valueOf("54");
+        Integer second = 45;
+    }
+}
